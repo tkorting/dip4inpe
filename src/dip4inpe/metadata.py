1fb28f7451147f8fb203829f5936b822e02a3884
Thales Sehn Körting <tkorting@gmail.com>
2026-08-13 15:43:23 -0300

    Update metadata.py

--- src/dip4inpe/metadata.py
+++ src/dip4inpe/metadata.py
@@ -1,5 +1,17 @@
 import os
-def get_raster_info_from_name(raster_path, separator='_'):
+def get_raster_info_from_name(raster_path, separator = '_'):
+    '''
+    Obtain dictionary with all metadata contained in GeoTIFF/XML filename
+    
+    parameters
+    ----------
+    raster_path: string with full path of GeoTIFF/XML file
+    separator: string with default value '_'
+    
+    return
+    ------
+    dictionary where keys = (satellite_name, satellite_number, sensor_name, acquisition_date, path, row, processing_level, extra, band_number) and values = strings with corresponding information
+    '''
     file_parts = os.path.basename(raster_path).split(separator)
     raster_info = {
         'satellite_name': file_parts[0],
@@ -15,7 +27,20 @@ def get_raster_info_from_name(raster_path, separator='_'):
     return raster_info
 
 from datetime import datetime
-def get_raster_description(raster_path, language='pt', separator='_'):
+def get_raster_description(raster_path, language = 'pt', separator = '_'):
+    '''
+    Obtain readable text information about raster
+    
+    parameters
+    ----------
+    raster_path: string with full path of GeoTIFF file
+    language: string with 'pt' for Portugues and 'en' for English
+    separator: string with default value '_'
+    
+    return
+    ------
+    string with text describing raster
+    '''
     raster_info = get_raster_info_from_name(raster_path, separator)
     date = datetime.strptime(raster_info['acquisition_date'], '%Y%m%d')
     if language == 'en':
@@ -28,12 +53,34 @@ def get_raster_description(raster_path, language='pt', separator='_'):
 
 import rasterio as rio
 def get_number_of_pixels(raster_path):
+    '''
+    Obtain total number of pixels in raster
+    
+    parameters
+    ----------
+    raster_path: string with full path of GeoTIFF file
+    
+    return
+    ------
+    integer with total number of pixels (matrix height x matrix width)
+    '''
     with rio.open(raster_path) as raster:
         return raster.height * raster.width
 
 import math
-def get_spatial_resolution_in_meters(raster_name):
-    with rio.open(raster_name) as raster:
+def get_spatial_resolution_in_meters(raster_path):
+    '''
+    Obtain the spatial resolution (in meters) of raster, considering x/y resolutions are equal
+    
+    parameters
+    ----------
+    raster_path: string with full path of GeoTIFF file
+    
+    return
+    ------
+    integer with total number of pixels (matrix height x matrix width)
+    '''
+    with rio.open(raster_path) as raster:
         if raster.crs.is_geographic:
             lat_center = (raster.bounds.top + raster.bounds.bottom) / 2
             # 1 degree latitude ~= 111.32 km
